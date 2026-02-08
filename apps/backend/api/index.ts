d0a099af54800501a5ef4a0f67c8dd1c472aee6b
Manoj Kumar <manojbhichchhar8@gmail.com>
2026-02-08 17:08:06 +0530

    vercel json12

--- apps/backend/api/index.ts
+++ apps/backend/api/index.ts
@@ -1,27 +1,36 @@
-// import http from 'http';
-import dotenv from 'dotenv'
-dotenv.config();
-import app from '../src/app';
-import mongoose from 'mongoose';
-import { asyncHandler } from '../src/utils/asyncHandler';
+// // import http from 'http';
+// import dotenv from 'dotenv'
+// dotenv.config();
+// import app from '../src/app';
+// import mongoose from 'mongoose';
+// import { asyncHandler } from '../src/utils/asyncHandler';
 
-const connectDb=async ()=>{
-    try{
-        console.log('connecting to db....');
+// const connectDb=async ()=>{
+//     try{
+//         console.log('connecting to db....');
         
-        const db=await mongoose.connect(process.env.DB_URI||'');
-        console.log('connected to db.');
-        // const server=http.createServer(app);
-        // server.listen(process.env.PORT,()=>{
-        //     console.log(`server listening at port ${process.env.PORT}`);
-        // })
-    }catch(error){
-        console.log("Error while connecting the MongoDB : ", error);
+//         const db=await mongoose.connect(process.env.DB_URI||'');
+//         console.log('connected to db.');
+//         // const server=http.createServer(app);
+//         // server.listen(process.env.PORT,()=>{
+//         //     console.log(`server listening at port ${process.env.PORT}`);
+//         // })
+//     }catch(error){
+//         console.log("Error while connecting the MongoDB : ", error);
         
-    }
-}
+//     }
+// }
 
-export default async function handler(req:any, res:any){
-    await connectDb();
-    return app(req,res);
+// export default async function handler(req:any, res:any){
+//     await connectDb();
+//     return app(req,res);
+// }
+
+
+import app from "../src/app";
+import { connectDB } from "../lib/db";
+
+export default async function handler(req: any, res: any) {
+  await connectDB();
+  return app(req, res);
 }
---
+++ apps/backend/lib/db.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const DB_URI = process.env.DB_URI;
+
+if (!DB_URI) {
+  throw new Error("DB_URI is missing");
+}
+
+let cached = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = {
+    conn: null,
+    promise: null,
+  };
+}
+
+export async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(DB_URI).then((mongoose) => {
+      return mongoose;
+    });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
