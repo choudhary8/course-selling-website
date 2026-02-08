3103642e4f82c83904ced760ac8d37604e8ca589
Manoj Kumar <manojbhichchhar8@gmail.com>
2026-02-08 14:25:17 +0530

    mongo fn

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import adminRouter from './routes/admin.routes';
 import userRouter from './routes/users.routes';
-// import { errorHandler } from './middlewares/error.middlewares';
+import { errorHandler } from './middlewares/error.middlewares';
 
 const app=express();
 
@@ -12,5 +12,6 @@ app.use(express.json());
 app.use('/api/v1/admin',adminRouter);
 app.use('/api/v1/users',userRouter);
 
+app.use(errorHandler as any)
 
 export default app;
--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -3,23 +3,21 @@ import dotenv from 'dotenv'
 dotenv.config();
 import app from './app';
 import mongoose from 'mongoose';
-import { errorHandler } from './middlewares/error.middlewares';
 
-
-
-try{
-    const connectDb=async ()=>{
+const connectDb=async ()=>{
+    try{
+        console.log('connecting to db....');
+        
         const db=await mongoose.connect(process.env.DB_URI||'');
-        console.log(db);
+        console.log('connected to db');
+        const server=http.createServer(app);
+        server.listen(process.env.PORT,()=>{
+            console.log(`server listening at port ${process.env.PORT}`);
+        })
+    }catch(error){
+        console.log("Error while connecting the MongoDB : ", error);
+        
     }
-    connectDb();
-    const server=http.createServer(app);
-    server.listen(process.env.PORT,()=>{
-        console.log(`server listening at port ${process.env.PORT}`);
-    })
-}catch(error){
-    console.log("Error while connecting the MongoDB : ", error);
-    
 }
 
-app.use(errorHandler as any)
+connectDb();
