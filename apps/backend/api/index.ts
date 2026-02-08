f440f07ac9e657a6080d40b08ae0a9e69bc5fda1
Manoj Kumar <manojbhichchhar8@gmail.com>
2026-02-08 16:43:17 +0530

    vercel json5

--- apps/backend/api/index.ts
+++ apps/backend/api/index.ts
@@ -1,7 +1,7 @@
 import http from 'http';
 import dotenv from 'dotenv'
 dotenv.config();
-import app from './app';
+import app from '../src/app';
 import mongoose from 'mongoose';
 
 const connectDb=async ()=>{
@@ -11,9 +11,9 @@ const connectDb=async ()=>{
         const db=await mongoose.connect(process.env.DB_URI||'');
         console.log('connected to db.');
         const server=http.createServer(app);
-        server.listen(process.env.PORT,()=>{
-            console.log(`server listening at port ${process.env.PORT}`);
-        })
+        // server.listen(process.env.PORT,()=>{
+        //     console.log(`server listening at port ${process.env.PORT}`);
+        // })
     }catch(error){
         console.log("Error while connecting the MongoDB : ", error);
         
