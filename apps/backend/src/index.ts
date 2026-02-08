1f2107fba8365bfc8de0d5554b72aab7f112f1f6
Manoj Kumar <manojbhichchhar8@gmail.com>
2026-02-08 22:44:33 +0530

    render deployment

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,28 @@
+import http from 'http';
+import dotenv from 'dotenv'
+dotenv.config();
+import app from '../src/app';
+import mongoose from 'mongoose';
+import { asyncHandler } from '../src/utils/asyncHandler';
+
+const connectDb=async ()=>{
+    try{
+        console.log('connecting to db....');
+        
+        const db=await mongoose.connect(process.env.DB_URI||'');
+        console.log('connected to db.');
+        const server=http.createServer(app);
+        server.listen(process.env.PORT,()=>{
+            console.log(`server listening at port ${process.env.PORT}`);
+        })
+    }catch(error){
+        console.log("Error while connecting the MongoDB : ", error);
+        
+    }
+}
+
+export default async function handler(req:any, res:any){
+    await connectDb();
+    return app(req,res);
+}
+
