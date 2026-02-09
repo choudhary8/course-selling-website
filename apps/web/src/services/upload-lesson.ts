29314af4e2c33df24cc208d8e91314e546d03ceb
Manoj Kumar <manojbhichchhar8@gmail.com>
2026-02-09 14:31:07 +0530

    loading handling2

--- apps/web/src/services/upload-lesson.ts
+++ apps/web/src/services/upload-lesson.ts
@@ -1,13 +1,12 @@
 import axios from "axios";
 import { BASE_URL } from "../utils/constants";
 import type { IlessonDetails } from "../utils/interfaces";
-import toast from "react-hot-toast";
 import { errorHandler } from "../utils/errorHandler";
 
 export const uploadLesson=async (lessonDetails:IlessonDetails)=>{
     try {
         const authToken=localStorage.getItem('authToken');
-        const res=await axios.post(`${BASE_URL}/admin/course/upload-lesson`,lessonDetails,{
+        await axios.post(`${BASE_URL}/admin/course/upload-lesson`,lessonDetails,{
             headers:{
                 Authorization: `Bearer ${authToken}`,
                 "Content-Type": "multipart/form-data"
