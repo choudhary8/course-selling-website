7f0fe2a5e4af8d4dbbdfe92b5462ff2ee2d8b07b
Manoj Kumar <manojbhichchhar8@gmail.com>
2026-02-10 21:47:27 +0530

    backpl

--- apps/web/src/utils/constants.ts
+++ apps/web/src/utils/constants.ts
@@ -1,3 +1,3 @@
-export const BASE_URL='http://51.21.198.46:3000/api/v1';
+// export const BASE_URL='http://localhost:3000/api/v1';
 // export const BASE_URL='https://course-hub-backend-henna.vercel.app/api/v1';
-// export const BASE_URL='https://coursehub-backend-w5f1.onrender.com/api/v1';
+export const BASE_URL='https://coursehub-backend-w5f1.onrender.com/api/v1';
